0a42c1b845de15ede5d57a2393c41b8de0efe3b3
Akashdubey512 <yogesh8853599651@gmail.com>
2025-12-11 17:31:28 +0000

    Connect database and make custom api responses

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { application } from "express";
 
 dotenv.config({
     path: './.env'
 })
 connectDB()
+.then(()=>{
+    application.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!!!",err);
+})
