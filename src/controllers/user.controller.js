0c9dc9751fc3e75e67ff72951f87c7e202e2cb30
Akash Dubey <yogesh8853599651@gmail.com>
2025-12-27 17:16:33 +0530

    Remove debug log from registerUser and revert filename format in multer middleware to original

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,7 +54,6 @@ const registerUser = asyncHandler( async (req, res) => {
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    console.log("files", req.files);
     // let coverImageLocalPath;
     // if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
     //     coverImageLocalPath = req.files.coverImage[0].path
--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -5,8 +5,7 @@ const storage=multer.diskStorage({
         cb(null,"./public/temp")
     },
     filename:function(req,file,cb){
-         cb(null, `${Date.now()}-${file.fieldname}-${file.originalname}`);
-    }
+    cb(null,file.originalname)    }
 })
 
 export const upload=multer({
